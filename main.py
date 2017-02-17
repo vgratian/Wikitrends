dfb6b20d76e66da0935b1ff012870d86498b8279
imandes0 <vgratian@utopianlab.am>
2017-02-17 20:23:11 +0400

    github link in result page

--- main.py
+++ main.py
@@ -16,10 +16,10 @@ def search():
     wikientry = WikiTrends(session['query'], 10)
     if wikientry.validate_article():
         search_result = wikientry.get_hits('html')
-        return render_template('results.html') + 'Search results from "%s" Wikipedia article:<br /><br />%s' % (escape(session['query']), search_result) + '</body>'
-    return render_template('results.html') + 'No article "%s" found in Wikipedia' % (escape(session['query'])) + '</body>'
+        return render_template('results.html') + 'Search results from "%s" Wikipedia article:<br /><br />%s' % (escape(session['query']), search_result) + render_template('tail.html')
+    return render_template('results.html') + 'No article "%s" found in Wikipedia' % (escape(session['query'])) + render_template('tail.html')
 
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=5000)
+    app.run(host='127.0.0.1',port=5000)
